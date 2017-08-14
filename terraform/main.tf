a1ab1d052e4c08e85446844041b33f9924c9c50a
Wei Sun <wwsun@CNwwsun.local>
2017-08-14 15:46:43 +0800

    add for deply automatically; Wayde

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,18 @@
 provider "google" {
   credentials = ""
-  project      = "example-project-wayde191"
-  region       = "us-central1-a"
+  project      = "asia-project-wayde191"
+  region       = "asia-northeast1"
 }
 
 resource "google_container_cluster" "primary" {
-  name         = "first-cluster"
-  zone         = "us-central1-a"
+  name         = "second-asia-cluster"
+  zone         = "asia-northeast1-a"
   initial_node_count = 1
+
+  additional_zones = [
+    "asia-northeast1-b",
+    "asia-northeast1-c",
+  ]
 
   logging_service = "logging.googleapis.com"
   monitoring_service = "monitoring.googleapis.com"
@@ -15,5 +20,4 @@ resource "google_container_cluster" "primary" {
   node_config {
     machine_type = "n1-standard-2"
   }
-
 }
